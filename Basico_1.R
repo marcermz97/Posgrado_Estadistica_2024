a101b9237650f72348c44612472b2599ba9a67f3
Marce Posgrado <marcermz_97@hotmail.com>
2024-09-20 11:46:45 -0600

    actualiod

---
+++ Basico_1.R
@@ -0,0 +1,20 @@
+#Marcela Rosario Ramírez González
+1742134
+14/ 08/ 2024
+
+
+
+# Primeros datos ----------------------------------------------------------
+
+2 + 2
+3 * 3
+
+altura <- c (8, 12, 3, 5, 9)
+altura
+
+# Interpretar los resultados por 3
+altura * 3
+
+
+# Segundos ----------------------------------------------------------------
+
