01931c81ae4fd0a00a6d0e83f9d2ead9b1cb2de9
Marce Posgrado <marcermz_97@hotmail.com>
2024-09-25 15:46:04 -0600

    examen

--- EXAMEN1.R
+++ EXAMEN1.R
@@ -1,12 +1,30 @@
-prod <- read.csv("tarea1.csv", header = T)
+prod <- read.csv("datosexamen", header = T)
 View(prod)
-summary(prod)
-
-
-
-
 
 datos <- data.frame(diam_arboles, altura_arboles)
+
+library(readxl)
+examen <- read.csv("examen.csv")
+View(examen)
+
+
+"https://github.com/mgtagle/Test-202/blob/master/Scripts/PrincipiosEstadistica.md"
+
+
+
+#Para reproductibilidad
+set.seed(42)
+n <- 30
+altura <- rnorm(n, mean = 170, sd = 10)
+peso <- 0.5 * altura + rnorm(n, mean = 0, sd = 5)
+
+
+
+#Para reproductibilidad
+set.seed(42) 
+n <- 30
+altura <- rnorm(n, mean = 170, sd = 10) #Altura en cm
+peso <- 0.5 * altura + rnorm(n, mean = 0, sd = 5)  #Peso en kg, con algo de ruido
 
 
 sitios<- read.csv("claseanova.csv", header = T)
@@ -17,3 +35,15 @@ n <-30
 altura <- rnorm(n, mean = 170, sd = 10) 
 peso <- 0.05 *altura + rnorm(n, mean = 0, sd = 5)
 View(altura)
+
+#Importar datos de archivo excel a la consola de R
+#Funcion "read.csv"
+setwd("C:/examen")
+
+clima <- read.csv("examen")
+head(clima)
+tail(e)
+
+
+
+
